08b3321ab040ceefcec71942d9909b795150d19d
asareri06\asareri <raj@asareri.com>
2017-06-30 18:49:59 +0530

    initial

---
+++ middleware.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var http = require('http');
+var cons = require('consolidate');
+var swig = require('swig');
+
+var swigFilters = require('./filters');
+var router = require('./router');
+
+var middleware = function(config) {
+      var app = express();
+
+  //Set up swig
+  app.engine('html', cons.swig);
+  Object.keys(swigFilters).forEach(function (name) {
+      swig.setFilter(name, swigFilters[name]);
+  });
+
+  //App configuration
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.set('view options', {layout: false});
+
+  app.use('/', router(config));
+
+  return app;
+}
+
+module.exports = middleware;
